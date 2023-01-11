71dec6824bcdf6fff6598368c1668b48668221db
terzdek <zdenekkolarusti@seznam.cz>
2023-01-11 23:17:26 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 // Load Map
 function init_map(){
     var map_obj = L.map('mapid').setView([45, 4], 5);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    L.tileLayer('https://mapserver.mapy.cz/turist-m/{z}-{x}-{y}.png?access_token={accessToken}', {
     attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
     tileSize: 512,
     maxZoom: 18,
